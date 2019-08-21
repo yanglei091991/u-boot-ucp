e2fb22ba754807ec9f2f8da2ee0b4b793e247ac8
yanglei <yanglei2013@ia.ac.cn>
2019-08-21 09:43:32 +0800

    yanglei:Simplifed uart-ok version.

--- include/configs/ucp_a53.h
+++ include/configs/ucp_a53.h
@@ -1,17 +1,86 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
-/*
- * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
- *
- * Configuration settings for the Freescale i.MX6Q Armadillo2 board.
- */
-
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#include "ucp_common.h"
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#define CONFIG_BOARD_POSTCLK_INIT
+
+#define CONFIG_SYS_BOOTM_LEN	0x1000000
+
+#include <linux/sizes.h>
+#define CONFIG_SYS_TIMER_COUNTER 0x031d0004
+#define CONFIG_SYS_TIMER_COUNTS_DOWN
+
+/* Boot options */
+//???
+#define CONFIG_LOADADDR		0x40000000
+//???
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
+
+/* allow to overwrite serial and ethaddr */
+//???
+#define CONFIG_ENV_OVERWRITE
+
+/* Miscellaneous configurable options */
+//???
+#define CONFIG_SYS_CBSIZE	512
+//???
+#define CONFIG_SYS_MAXARGS	32
+
+/* MMC */
+#define CONFIG_BOUNCE_BUFFER
+
+// yanglei
+#define IRAM_BASE_ADDR      			0x10000000
+#define IRAM_SIZE                       0x00100000
+//#define ROMCP_ARB_BASE_ADDR             0x00000000
+//#define ROMCP_ARB_END_ADDR              0x000FFFFF
+
+// u-boot size
+//???
+#if 0
+#define CONFIG_SYS_MONITOR_BASE CONFIG_SYS_FLASH_BASE
+#define CONFIG_SYS_FLASH_BASE 0x00000000
+#endif
+
+
+/* NS16550 Serial Configuration */
+#define CONFIG_SYS_NS16550
+#define CONFIG_SYS_NS16550_SERIAL
+#define CONFIG_SYS_NS16550_REG_SIZE	(-4)
+#define CONFIG_CONS_INDEX		1
+#define CONFIG_SYS_NS16550_CLK		25000000   /* 41200000 */
+#define CONFIG_SYS_NS16550_COM1   0x02108000 /* 0x50920000, Base EVM has UART0 */ /*Match MaPU chip*/
+#define CONFIG_BAUDRATE		115200
+#define CONFIG_SYS_BAUDRATE_TABLE  \
+    {300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400}
+#define CONFIG_SYS_NS16550_MEM32
+
+/* Physical Memory Map */
+
+#define CONFIG_SYS_SDRAM_BASE		0x20000000
+#define PHYS_SDRAM_SIZE             (0x4FFFFFFF - CONFIG_SYS_SDRAM_BASE)
+//#define PHYS_SDRAM_SIZE             (0x7FFFFFFF - CONFIG_SYS_SDRAM_BASE)
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+/* Environment organization */
+#define CONFIG_ENV_OFFSET		(6 * 64 * 1024)
+#define CONFIG_ENV_SIZE			(8 * 1024)
+//#define CONFIG_SYS_MMC_ENV_DEV		1
 
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 2 * 1024 * 1024)
+
+
+#if 0
+
+
+#define CONFIG_ARP_TIMEOUT	200UL
 
 /* MMC Configs */
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC4_BASE_ADDR
@@ -94,27 +163,10 @@
 		"fi; " \
 	"else run netboot; fi"
 
-#define CONFIG_ARP_TIMEOUT	200UL
-
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_MEMTEST_START	0x10000000
 #define CONFIG_SYS_MEMTEST_END		0x10010000
+#endif
 
-/* Physical Memory Map */
-#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
-
-#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
-#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
-#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
-
-#define CONFIG_SYS_INIT_SP_OFFSET \
-	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
-#define CONFIG_SYS_INIT_SP_ADDR \
-	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
-
-/* Environment organization */
-#define CONFIG_ENV_OFFSET		(6 * 64 * 1024)
-#define CONFIG_ENV_SIZE			(8 * 1024)
-#define CONFIG_SYS_MMC_ENV_DEV		1
 
 #endif				/* __CONFIG_H */
