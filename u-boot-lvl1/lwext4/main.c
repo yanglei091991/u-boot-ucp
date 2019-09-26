ce6bcd2306f81201df9c8eded9af405d395174d5
Iru Cai <mytbk920423@gmail.com>
2019-09-26 19:18:59 +0800

    add a test

---
+++ u-boot-lvl1/lwext4/main.c
@@ -0,0 +1,27 @@
+#include <ext4.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "file_dev.h"
+
+#define BUFSZ 128
+
+int main()
+{
+	ext4_file bootfile;
+	uint8_t buffer[BUFSIZ];
+	size_t size;
+
+	file_dev_name_set("/tmp/fs.img");
+	struct ext4_blockdev *blk = file_dev_get();
+	ext4_device_register(blk, "mmc");
+
+	ext4_mount("mmc", "/", true /* read only */);
+
+	ext4_fopen(&bootfile, "/boot.txt", "rb");
+	ext4_fread(&bootfile, buffer, BUFSZ, &size);
+	for (size_t i = 0; i < size; i++)
+		putchar(buffer[i]);
+
+	ext4_fclose(&bootfile);
+	ext4_umount("/");
+}
