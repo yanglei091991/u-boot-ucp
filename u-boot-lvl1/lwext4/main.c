3cc6c67bf309d02a419cbf4c515652286b777476
Iru Cai <mytbk920423@gmail.com>
2019-09-29 16:11:58 +0800

    try to use user_malloc

--- u-boot-lvl1/lwext4/main.c
+++ u-boot-lvl1/lwext4/main.c
@@ -5,11 +5,13 @@
 
 #define BUFSZ 128
 
+void printptr(void);
+
 int main()
 {
 	ext4_file bootfile;
 	uint8_t buffer[BUFSIZ];
-	size_t size;
+	size_t size = -1;
 
 	file_dev_name_set("/tmp/fs.img");
 	struct ext4_blockdev *blk = file_dev_get();
@@ -18,10 +20,15 @@ int main()
 	ext4_mount("mmc", "/", true /* read only */);
 
 	ext4_fopen(&bootfile, "/boot.txt", "rb");
-	ext4_fread(&bootfile, buffer, BUFSZ, &size);
-	for (size_t i = 0; i < size; i++)
-		putchar(buffer[i]);
+
+	while (size) {
+		ext4_fread(&bootfile, buffer, BUFSZ, &size);
+		for (size_t i = 0; i < size; i++)
+			putchar(buffer[i]);
+	}
 
 	ext4_fclose(&bootfile);
 	ext4_umount("/");
+	puts("\nbuffer size used:");
+	printptr();
 }
